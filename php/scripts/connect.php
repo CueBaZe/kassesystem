883621e938f2aa5d12122923d57020fa42414f7b
CueBaZe <olivervalente@icloud.com>
2025-02-25 08:07:18 +0100

    created connect.php

---
+++ php/scripts/connect.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "kasse";
+
+$conn = new mysqli($host, $user, $password, $db);
+
+if(!$conn) {
+    die("Connection failed: " . mysqli_connect_error);
+}
+
+if(conn->connect_error) {
+    die("Failed to connect to DB: " . $conn->connect_error);
+}
+?>
