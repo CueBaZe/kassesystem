2d5e31f4ae1afa6885adcf7d0004d7e415e7a571
CueBaZe <olivervalente@icloud.com>
2025-02-25 10:50:48 +0100

    Added a simple footer

---
+++ elements/footer.php
@@ -0,0 +1,13 @@
+<div class="container-fluid" name="footer" id="footer">
+    <div class="row">
+        <p class="col-6">Items:</p>
+        <div class="buttonBox col-6">
+
+        </div>
+        <p class="col-12">Price:</p>
+    </div>
+</div>
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</html>
--- elements/nav.php
+++ elements/nav.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/nav_style.css">
+    <link rel="stylesheet" href="style/footer_style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Kassesystem</title>
 </head>
 <body>
-    <div class="container-fluid">
+    <div class="container-fluid" name="nav" id="nav">
         <div class="row">
             <nav class="navbar fixed-top py-3 col-12">
                 <a class="navbar-brand logo" href="#">Kasse</a>
