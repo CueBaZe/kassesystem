013ceadfa2636c3eee00de8a51a78b10b133e2d9
CueBaZe <olivervalente@icloud.com>
2025-02-26 08:45:23 +0100

    Removed clear button from footer

--- elements/footer.php
+++ elements/footer.php
@@ -1,13 +1,12 @@
 <div class="container-fluid" name="footer" id="footer">
     <div class="row">
         <div class="row col-6">
-            <p class="col-12">Items:</p>
-            <p class="col-12">Price:</p>
+            <p class="col-12">Items: 10</p>
+            <p class="col-12">Price: 73$</p>
         </div>
         <div class="row col-6">
-            <div class="buttonBox">
-                <button type="button" onclick="" class="btn clear col-5 m-4">Clear</button>
-                <button type="button" onclick="" class="btn buy col-5 m-4">Buy</button>
+            <div class="buttonBox text-end">
+                <button type="button" onclick="" class="btn buy col-6 m-4">Buy</button>
             </div>
         </div>
     </div>
