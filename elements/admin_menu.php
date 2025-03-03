55c29d51dbf29acf4065a5df7ab891c48bb3840d
CueBaZe <olivervalente@icloud.com>
2025-03-03 14:39:04 +0100

    styled admin menu

--- elements/admin_menu.php
+++ elements/admin_menu.php
@@ -1,8 +1,10 @@
 <div class="container disabled" id="adminmenu" disabled="true">
     <div class="row">
         <div class="wrapper col-8" id="admin_wrapper">
-            <div class="row">
-                <h3>Admin Menu</h3>
+            <div class="row text-center justify-content-center">
+                <h3 class="col-12">Admin Menu</h3>
+                <button class="btn itembtn col-6">Add items</button>
+                <button class="btn discountbtn col-6">Discounts</button>
             </div>
         </div>
     </div>
