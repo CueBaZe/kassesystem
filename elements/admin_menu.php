4f22e430c75a9fb62c256e5981adaae4446b93b3
CueBaZe <olivervalente@icloud.com>
2025-03-04 08:29:15 +0100

    fixed admin menu screen sizing

--- elements/admin_menu.php
+++ elements/admin_menu.php
@@ -1,11 +1,10 @@
 <div class="container disabled" id="adminmenu" disabled="true">
-    <div class="row">
-        <div class="wrapper col-8" id="admin_wrapper">
-            <div class="row text-center justify-content-center">
-                <h3 class="col-12">Admin Menu</h3>
-                <button class="btn itembtn col-6">Add items</button>
-                <button class="btn discountbtn col-6">Discounts</button>
-            </div>
+    <div class="wrapper" id="admin_wrapper">
+        <div class="row text-center justify-content-center">
+            <h3 class="col-12">Admin Menu</h3>
+            <button class="btn itembtn col-10 col-sm-10 col-md-5">Add items</button>
+            <button class="btn discountbtn col-10 col-sm-10 col-md-5">Discounts</button>
         </div>
     </div>
 </div>
+
