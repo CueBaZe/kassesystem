f19f8e2b8fd17771873a6cd8384f413506ace2a7
CueBaZe <olivervalente@icloud.com>
2025-03-11 10:54:32 +0100

    changed footer code sturture

--- elements/footer.php
+++ elements/footer.php
@@ -1,36 +1,34 @@
 <?php
-$cart = $_SESSION['cart'];
+session_start();
+include("connect.php");
+
+$cart = $_SESSION['cart'] ?? [];
+
+$items = 0;
+$price = 0;
+
+foreach ($cart as $item => $numberOfItems) {
+    $items += $numberOfItems;
+    
+    $stmt = $conn->prepare("SELECT price FROM items WHERE barcode = ?");
+    $stmt->bind_param("i", $item);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $stmt->close();
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $price += $row['price'] * $numberOfItems;
+        }
+    }
+}
 ?>
+
 <div class="container-fluid" name="footer" id="footer">
     <div class="row">
         <div class="row col-6">
-            <p class="col-12">Items: 
-            <?php
-            //Checks how many items you have in your cart
-            $items = 0;
-            foreach($cart as $item => $numberOfItems) {
-                $items += $numberOfItems;
-            }
-            echo $items;
-            ?></p>
-            <p class="col-12">Price:     
-            <?php
-            $price = 0;
-            foreach($cart as $item => $numberOfItems) {
-                $stmt = $conn->prepare("SELECT * FROM items WHERE barcode = ?");
-                $stmt->bind_param("i", $item);
-                $stmt->execute();
-                $result = $stmt->get_result();
-                $stmt->close();
-
-                if($result->num_rows > 0) {
-                    while($row = $result->fetch_assoc()) {
-                        $price += $row['price'] * $numberOfItems;
-                    }
-                }
-            }
-            echo $price;
-            ?>$</p>
+            <p class="col-12">Items: <?php echo $items; ?></p>
+            <p class="col-12">Price: <?php echo $price; ?>$</p>
         </div>
         <div class="row col-6">
             <div class="buttonBox text-end">
@@ -39,7 +37,7 @@ $cart = $_SESSION['cart'];
         </div>
     </div>
 </div>
-</body>
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="php/scripts/timechecker.js"></script>
