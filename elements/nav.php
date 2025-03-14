6ff183543998abe93467dfd65e9ad3d7de46f382
CueBaZe <olivervalente@icloud.com>
2025-03-14 11:00:09 +0100

    Made a edit button show if your an admin

--- elements/nav.php
+++ elements/nav.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="style/admin_menu_style.css">
     <link rel="stylesheet" href="style/item.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>Kassesystem</title>
 </head>
 <body>
--- index.php
+++ index.php
@@ -26,7 +26,16 @@
             <div class="col-12 col-sm-6 col-md-4 col-lg-3 d-flex">
                 <div class="boxes p-4 mt-4 text-center">
                     <div class="row justify-content-center">
-                        <img src="./images/<?php echo $data['picture_path']; ?>" class="img-fluid" alt="Uploaded Image">
+                        <?php
+                        if($_SESSION['role'] == "admin") {
+                        ?>
+                        <form class="text-end mb-2" action="elements/edititem_page.php" method="POST">
+                            <button class="editItem col-2" data-bs-toggle="tooltip" title="Edit item" name="editItem" value="<?php echo $data['barcode']?>"><i class='bx bx-pencil'></i></button>
+                        </form>
+                        <?php
+                        }
+                        ?>
+                        <img src="./images/<?php echo $data['picture_path']; ?>" class="img-fluid col-12" alt="Uploaded Image">
                         <h3 class="item-name col-12"><?php echo $data['name']; ?></h3>
                         <p class="item-price col-12">Price: <?php echo $data['price']; ?> $</p>
                         <form action="php/scripts/addToCart.php" method="POST">
@@ -35,12 +44,12 @@
                             <?php
                             if($_SESSION['role'] == "admin") {
                             ?>
-                        <form action="php/scripts/delete.php" method="POST">
-                                <button class="deleteItem col-8" name="deleteItem" value="<?php echo $data['barcode']; ?>">Delete Item</button>
-                        </form>
-                        <?php
-                        }
-                        ?>
+                                <form action="php/scripts/delete.php" method="POST">
+                                    <button class="deleteItem col-8" name="deleteItem" value="<?php echo $data['barcode']; ?>">Delete Item</button>
+                                </form>
+                            <?php
+                            }
+                            ?>
                         
                         <p class="item-barcode text-end col-12"><?php echo $data['barcode'];?></p>
                     </div>
